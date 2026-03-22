4e433b17c4a12aaa8022e2633c38992917a1d6cd
Dinesh2610m <dinesh2610m@gmail.com>
2026-03-22 17:12:41 +0000

    Added disk and CPU info

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import os
 import platform
+import shutil
 
-print("=== System Info Tool ===")
+print("=== System Info Tool ===\n")
 
 # Username
 print("User:", os.getlogin())
@@ -11,3 +12,13 @@ print("OS:", platform.system(), platform.release())
 
 # Machine name
 print("Machine:", platform.node())
+
+# CPU info
+print("Processor:", platform.processor())
+
+# Disk usage
+total, used, free = shutil.disk_usage("/")
+print("\nDisk Usage:")
+print("Total:", total // (2**30), "GB")
+print("Used:", used // (2**30), "GB")
+print("Free:", free // (2**30), "GB")
